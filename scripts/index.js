259fcd6d37fff8447b643ffabdbd4e110be839e4
Roland Askew <rolandjamesaskew37@gmail.com>
2021-07-15 13:40:54 +1200

    reet button. iter #3

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,18 @@
 let puzzle
 let puzzleHidden
 
-window.onload = async function () {
+const reRenderPuzzle = async function () {
   const wordCount = Math.ceil(1 + Math.random() * 4)
   puzzle = await getPuzzle(wordCount)
   puzzleHidden = puzzle
   render(puzzleHidden)
+}
+
+const clickReset = async function () {
+  reRenderPuzzle()
+}
+
+window.onload = async function () {
+  reRenderPuzzle()
+  document.querySelector('#reset').addEventListener('click', clickReset)
 }
