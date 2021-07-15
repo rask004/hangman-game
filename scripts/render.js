ecbcfd23dc3c7394b85151614fffd40d6ad19f3f
Roland Askew <rolandjamesaskew37@gmail.com>
2021-07-15 13:13:27 +1200

    stylized layout for downloaded phrase. Iter #2

--- scripts/render.js
+++ scripts/render.js
@@ -1,4 +1,10 @@
 const render = function (phrase) {
-  const phraseLayout = document.querySelector('body')
-  phraseLayout.innerHTML = phrase
+  const container = document.querySelector('.container')
+  container.innerHTML = ''
+  for (const letter of phrase) {
+    const element = document.createElement('div')
+    element.classList.add('letter-box')
+    element.innerHTML = letter
+    container.append(element)
+  }
 }
